b15082442eca3b30605004c4d48cebf4f1263317
DanielSerio <dserio.d@gmail.com>
2022-05-26 08:19:18 -0400

    initial setup

---
+++ src/Theme.tsx
@@ -0,0 +1,15 @@
+import { MantineThemeOverride } from '@mantine/core';
+
+const Theme: MantineThemeOverride = {
+  fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+  sans-serif`,
+  lineHeight: 1.62,
+  primaryColor: 'cyan',
+  headings: {
+    fontFamily: `Cambria, Cochin, Georgia, Times, 'Times New Roman', serif`,
+    fontWeight: 200,
+  },
+};
+
+export default Theme;
--- src/index.tsx
+++ src/index.tsx
@@ -17,3 +17,17 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//TODO: SETUP:
+// Implement ColorSchemeProvider & MantineProvider
+//TODO: Component: Page
+//TODO: Component: Header
+//TODO: Component: Footer
+//TODO: Component: ActionIcon (for theme toggle)
+//TODO: Component: PageSection
+//TODO: Component: TypographyContainer
+//TODO: Component: PricingCard
+//TODO: Component: Logo
+//TODO: Page: Pricing/Index
+//TODO: Page: About
+//TODO: Page: Contact
