c2c34f8c16ac2fe4cf512c39e1e1094aea51e2f4
DanielSerio <dserio.d@gmail.com>
2022-05-26 12:50:20 -0400

    smaller footer text

--- src/components/layout/PageFooter.tsx
+++ src/components/layout/PageFooter.tsx
@@ -5,7 +5,7 @@ export default function PageFooter() {
   return (
     <Footer height={48} sx={{ display: 'grid', placeItems: 'center' }}>
       <Container sx={{ textAlign: 'center' }}>
-        <Text size={'sm'} color={'dimmed'}>
+        <Text size={'xs'} color={'dimmed'}>
           Dan Serio | 2022 |{' '}
           <Anchor href={'mailto:dserio.d@gmail.com'}>dserio.d@gmail.com</Anchor>
         </Text>
