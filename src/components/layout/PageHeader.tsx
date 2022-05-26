f781268dfb84f0228782d3558374afa2be9f6103
DanielSerio <dserio.d@gmail.com>
2022-05-26 10:02:37 -0400

    added PageHeader styles

--- src/components/layout/PageHeader.tsx
+++ src/components/layout/PageHeader.tsx
@@ -1,11 +1,26 @@
-import { Box, Header } from '@mantine/core';
+import { Box, createStyles, Header, MantineTheme } from '@mantine/core';
 import React from 'react';
+import { MantineStylesObject } from '../../types';
 import Logo from '../Logo';
 
+function createHeaderStyles(t: MantineTheme): MantineStylesObject {
+  return {
+    root: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+  };
+}
+
+const useHeaderStyles = createStyles(createHeaderStyles);
+
 export default function PageHeader() {
+  const { classes } = useHeaderStyles();
+
   return (
     <Box component='header'>
-      <Header height={72}>
+      <Header px={'md'} height={72} className={classes.root}>
         <Box>
           <Logo />
         </Box>
